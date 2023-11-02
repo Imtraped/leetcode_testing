a4057ba246827626fe4b99ee2dd334e6e10b3e8b
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-11-02 08:35:02 -0400

    Counter is done!

--- 30_days_of_JS/src.js
+++ 30_days_of_JS/src.js
@@ -1,5 +1,19 @@
+/* 2620. Counter */
+var createCounter = function(n) {
+    let count = n;
+
+    return function() {
+        return count++;
+    };
+};
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
 /* 2667. Create Hello World Function */
 var createHelloWorld = () => (...args) => `Hello World`;
 
 const persona = createHelloWorld();
-console.log(persona());
+persona();
