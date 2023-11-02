e83789d038b02b7f11b3cc92dbed4b17110180e2
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-11-02 11:00:42 -0400

    Nevermind I'm not there yet.

--- 30_days_of_JS/src.js
+++ 30_days_of_JS/src.js
@@ -7,20 +7,6 @@ var createCounter = function(n) {
     };
 };
 
-/* 2666. Allow One Function Call */
-var once = function(fn) {
-    let result;
-    let is_it_called = false;
-
-    return function(...args) {
-        if (!is_it_called) {
-            result = fn(...args);
-            is_it_called = true;
-            return result;
-        };
-    };
-};
-
 
 /* 2667. Create Hello World Function */
 var createHelloWorld = () => (...args) => `Hello World`;
