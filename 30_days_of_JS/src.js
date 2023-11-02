e00fbd2c839e87d66c9ee12c29aad5a4c787024f
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-11-02 09:38:15 -0400

    Allow One Function Call Done!

--- 30_days_of_JS/src.js
+++ 30_days_of_JS/src.js
@@ -7,13 +7,20 @@ var createCounter = function(n) {
     };
 };
 
-const counter = createCounter(10);
-console.log(counter());
-console.log(counter());
-console.log(counter());
+/* 2666. Allow One Function Call */
+var once = function(fn) {
+    let result;
+    let is_it_called = false;
+
+    return function(...args) {
+        if (!is_it_called) {
+            result = fn(...args);
+            is_it_called = true;
+            return result;
+        };
+    };
+};
+
 
 /* 2667. Create Hello World Function */
 var createHelloWorld = () => (...args) => `Hello World`;
-
-const persona = createHelloWorld();
-persona();
